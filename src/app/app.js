7ff5738d3640718645a3bacab400a3807321f811
Kacper <barabasz64@gmail.com>
2021-08-06 17:18:21 +0200

    Add locomotive-scroll library

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,19 @@
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import LocomotiveScroll from 'locomotive-scroll';
+
+const navbarLogo = document.querySelector('.navbar-logo');
+const hero = document.querySelector('.hero');
 const slides = document.querySelectorAll('.slide');
 
 const global = {
     slideIndex: 0
 }
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true
+});
 
 setInterval(() => {
     slides.forEach(e => e.style.opacity = 0)
@@ -12,3 +23,18 @@ setInterval(() => {
 
     if (global.slideIndex === 3) global.slideIndex = 0;
 }, 7000)
+
+
+const shrinkNav = function(entries) {
+    const [entry] = entries;
+
+    if (entry.isIntersecting) navbarLogo.style.maxHeight = '4.6rem';
+    else navbarLogo.style.maxHeight = '3rem';
+}
+
+const navObserver = new IntersectionObserver(shrinkNav, {
+    root: null,
+    threshold: 0.95
+})
+
+navObserver.observe(hero)
