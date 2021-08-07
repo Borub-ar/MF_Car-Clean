b1219b23dd619e4e2d29b5627a4f52b6fb2f064f
Kacper <kacper.barabasz@wp.pl>
2021-08-07 21:16:34 +0200

    Add offer section

--- src/app/app.js
+++ src/app/app.js
@@ -2,9 +2,10 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import LocomotiveScroll from 'locomotive-scroll';
 
-const navbarLogo = document.querySelector('.navbar-logo');
-const hero = document.querySelector('.hero');
+// const navbarLogo = document.querySelector('.navbar-logo');
+// const hero = document.querySelector('.hero');
 const slides = document.querySelectorAll('.slide');
+const offerSecInner = document.querySelector('.offer-inner');
 
 const global = {
     slideIndex: 0
@@ -25,16 +26,29 @@ setInterval(() => {
 }, 7000)
 
 
-const shrinkNav = function(entries) {
+// const shrinkNav = function(entries) {
+//     const [entry] = entries;
+
+//     if (entry.isIntersecting) navbarLogo.style.maxHeight = '4.4rem';
+//     else navbarLogo.style.maxHeight = '3rem';
+// }
+
+// const navObserver = new IntersectionObserver(shrinkNav, {
+//     root: null,
+//     threshold: 0.95
+// })
+
+// navObserver.observe(hero)
+
+const showSection = function(entries) {
     const [entry] = entries;
 
-    if (entry.isIntersecting) navbarLogo.style.maxHeight = '4.6rem';
-    else navbarLogo.style.maxHeight = '3rem';
-}
+    if (entry.isIntersecting) offerSecInner.classList.add('offer-inner-show');
+};
 
-const navObserver = new IntersectionObserver(shrinkNav, {
+const sectionObserver = new IntersectionObserver(showSection, {
     root: null,
-    threshold: 0.95
+    threshold: 0.1
 })
 
-navObserver.observe(hero)
+sectionObserver.observe(offerSecInner)
