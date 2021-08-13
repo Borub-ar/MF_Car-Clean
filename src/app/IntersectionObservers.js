13a7c94ea83eb117e54d5191e8ba649363f2d3b3
Kacper <kacper.barabasz@wp.pl>
2021-08-13 16:37:27 +0200

    Add form styling

---
+++ src/app/IntersectionObservers.js
@@ -0,0 +1,47 @@
+const navbarLogo = document.querySelector('.navbar-logo');
+const hero = document.querySelector('.hero');
+const offerSection = document.querySelector('.offer-inner');
+const aboutSection = document.querySelector('.about-section');
+const aboutCellText = document.querySelectorAll('.cell-text');
+
+const shrinkNav = function (entries) {
+  const [entry] = entries;
+
+  if (entry.isIntersecting) navbarLogo.style.maxHeight = '4.4rem';
+  else navbarLogo.style.maxHeight = '3rem';
+};
+
+const navObserver = new IntersectionObserver(shrinkNav, {
+  root: null,
+  threshold: 0.95,
+});
+
+navObserver.observe(hero);
+
+const showFirstSection = function (entries) {
+  const [entry] = entries;
+
+  if (entry.isIntersecting) offerSection.classList.add('offer-inner-show');
+};
+
+const firstSectionObserver = new IntersectionObserver(showFirstSection, {
+  root: null,
+  threshold: 0.2,
+});
+
+firstSectionObserver.observe(offerSection);
+
+const showSecondSection = function (entries) {
+  const [entry] = entries;
+
+  if (entry.isIntersecting) {
+    aboutCellText.forEach((cell) => cell.children.forEach((child) => child.classList.add('cell-text-animation')));
+  }
+};
+
+const secondSectionObserver = new IntersectionObserver(showSecondSection, {
+  root: null,
+  threshold: 0.2,
+});
+
+secondSectionObserver.observe(aboutSection);
