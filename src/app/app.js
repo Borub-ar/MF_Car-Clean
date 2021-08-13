2f78fbbe1c522bfdbfe0ab8eff9a75db4e0900e1
Kacper <barabasz64@gmail.com>
2021-08-13 13:17:42 +0200

    Add contact form

--- src/app/app.js
+++ src/app/app.js
@@ -3,8 +3,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import LocomotiveScroll from 'locomotive-scroll';
-import PhotoSwipe from 'photoswipe';
-
+// import PhotoSwipe from 'photoswipe';
 
 const navbarLogo = document.querySelector('.navbar-logo');
 const hero = document.querySelector('.hero');
@@ -12,47 +11,50 @@ const slides = document.querySelectorAll('.slide');
 const offerSecInner = document.querySelector('.offer-inner');
 
 const global = {
-    slideIndex: 0
-}
+  slideIndex: 0,
+};
 
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
+});
+
+window.addEventListener('load', () => {
+  locoScroll.update();
 });
 
 setInterval(() => {
-    slides.forEach(e => e.style.opacity = 0)
-    slides[global.slideIndex].style.opacity = 1;
+  slides.forEach((e) => (e.style.opacity = 0));
+  slides[global.slideIndex].style.opacity = 1;
 
-    global.slideIndex++;
+  global.slideIndex++;
 
-    if (global.slideIndex === 3) global.slideIndex = 0;
-}, 4000)
+  if (global.slideIndex === 3) global.slideIndex = 0;
+}, 4000);
 
+const shrinkNav = function (entries) {
+  const [entry] = entries;
 
-const shrinkNav = function(entries) {
-    const [entry] = entries;
-
-    if (entry.isIntersecting) navbarLogo.style.maxHeight = '4.4rem';
-    else navbarLogo.style.maxHeight = '3rem';
-}
+  if (entry.isIntersecting) navbarLogo.style.maxHeight = '4.4rem';
+  else navbarLogo.style.maxHeight = '3rem';
+};
 
 const navObserver = new IntersectionObserver(shrinkNav, {
-    root: null,
-    threshold: 0.95
-})
+  root: null,
+  threshold: 0.95,
+});
 
 navObserver.observe(hero);
 
-const showSection = function(entries) {
-    const [entry] = entries;
+const showSection = function (entries) {
+  const [entry] = entries;
 
-    if (entry.isIntersecting) offerSecInner.classList.add('offer-inner-show');
+  if (entry.isIntersecting) offerSecInner.classList.add('offer-inner-show');
 };
 
 const sectionObserver = new IntersectionObserver(showSection, {
-    root: null,
-    threshold: 0.1
-})
+  root: null,
+  threshold: 0.1,
+});
 
 sectionObserver.observe(offerSecInner);
