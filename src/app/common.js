aae900a7f829f298667e4bea7d0d0bece636d38e
Kacper <kacper.barabasz@wp.pl>
2021-08-18 12:32:51 +0200

    Change 'prices page' design

--- src/app/common.js
+++ src/app/common.js
@@ -1,8 +1,20 @@
 const navbarLogo = document.querySelector('.navbar-logo');
 const hero = document.querySelector('.hero');
 const navigation = document.querySelector('.navbar');
-// const navToggleBtnWrap = document.querySelector('.toggle-btn-wrap');
 const navToggleBtn = document.querySelector('.nav-toggle-btn');
+const toggleMenuCheckbox = document.getElementById('menu-checkbox');
+
+export const uncheckToggleMenu = function () {
+  toggleMenuCheckbox.checked = false;
+};
+
+export const hideNavbar = function () {
+  navigation.classList.remove('show-navbar');
+};
+
+const toggleNav = function () {
+  navigation.classList.toggle('show-navbar');
+};
 
 const shrinkNav = function (entries) {
   const [entry] = entries;
@@ -17,9 +29,4 @@ const navObserver = new IntersectionObserver(shrinkNav, {
 });
 
 navObserver.observe(hero);
-
-const toggleNav = function () {
-  navigation.classList.toggle('show-navbar');
-};
-
 navToggleBtn.addEventListener('click', toggleNav);
