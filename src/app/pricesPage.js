5f10f5a321e2f4fbd06f0ab4bb16370f2f745d7a
Kacper <barabasz64@gmail.com>
2021-08-18 22:05:23 +0200

    Add pages animations, refacor JS code.

--- src/app/pricesPage.js
+++ src/app/pricesPage.js
@@ -7,9 +7,28 @@ import { uncheckToggleMenu, hideNavbar } from './common';
 
 const contactNavItem = document.querySelector('.contact-item');
 const footer = document.querySelector('footer');
+const pricesSection = document.querySelector('.prices-section')
 
 contactNavItem.addEventListener('click', () => {
   footer.scrollIntoView({ behavior: 'smooth' });
   hideNavbar();
   uncheckToggleMenu();
 });
+
+
+
+const showPrices = function (entries) {
+  const [entry] = entries;
+
+  if (entry.isIntersecting) {
+    // gallerySectionInner.classList.add('section-in-view');
+    pricesSection.children.forEach(el => el.classList.add('section-in-view'))
+  }
+};
+
+const pricesObs = new IntersectionObserver(showPrices, {
+  root: null,
+  threshold: 0.2,
+});
+
+pricesObs.observe(pricesSection);
